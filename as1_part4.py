22234d03f628c08cc6a3e59736dbfe381560b7a2
hetu <patelh29@myumanitoba.ca>
2025-02-04 02:06:42 -0600

    part 4 completed

--- as1_part4.py
+++ as1_part4.py
@@ -8,26 +8,35 @@ def cal_R(left_speed, right_speed):
     return ( WHEEL_BASE_MM / 2) * ( ( left_speed + right_speed ) / ( right_speed - left_speed ))
 
 def diff_drive_inverse_kin(distance_mm, speed_mm_s, omega_rad):
+    
     if distance_mm == 0:
-        pass
-    else:
-        time_to_travel = abs(distance_mm) / speed_mm_s # in s
-        turn_rate = omega_rad / time_to_travel # omega w
+        distance_mm = round(omega_rad * WHEEL_DIAMETER / 2)
+        
+        distance_steps = mm_to_steps(distance_mm)
+        speed_steps_s = abs(mm_to_steps(speed_mm_s)) * (omega_rad / abs(omega_rad))
+        
+        return (int(-1 * speed_steps_s), int(speed_steps_s), int(abs(distance_steps)), int(abs(distance_steps)))
+    
+    time_to_travel = abs(distance_mm / speed_mm_s) # in s
+    turn_rate = omega_rad / time_to_travel # omega w
 
-        # Left and right wheel speeds in steps/s
-        left_mm_s = speed_mm_s - turn_rate * ( WHEEL_BASE_MM / 2 )
-        right_mm_s = speed_mm_s + turn_rate * ( WHEEL_BASE_MM / 2 )
+    # Left and right wheel speeds in mm/s
+    left_mm_s = speed_mm_s - turn_rate * ( WHEEL_BASE_MM / 2 )
+    right_mm_s = speed_mm_s + turn_rate * ( WHEEL_BASE_MM / 2 )
 
-        # Velocities in steps/s
-        left_steps_s = mm_to_steps(left_mm_s)
-        right_steps_s = mm_to_steps(right_mm_s)
+    # Velocities in steps/s
+    left_steps_s = mm_to_steps(left_mm_s)
+    right_steps_s = mm_to_steps(right_mm_s)
 
-        # Distance moved with velocities found * time to travel ? 
-        ### Need help with this one
-        left_steps = left_steps_s * time_to_travel
-        right_steps = right_steps_s * time_to_travel
+    # Distance moved with velocities found * time to travel ? 
+    ### Need help with this one
+    left_steps = abs(left_steps_s * time_to_travel)
+    right_steps = abs(right_steps_s * time_to_travel)
 
-        return (int(left_steps_s), int(right_steps_s), int(left_steps), int(right_steps))
+    return (int(left_steps_s), int(right_steps_s), int(left_steps), int(right_steps))
+
+# def diff_drive_inverse_kin2(distance_mm, speed_mm_s, omega_rad):
+    
 
 def main():
     # Test case
